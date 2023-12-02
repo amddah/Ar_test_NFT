14c17cbdfa823c6a6624f1afcf71d663e2ae2b4e
Abdelkbir AMDDAH <abdoamdah3@gmail.com>
2023-12-02 17:08:51 +0100

    First test

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const app = express();
+const path = require('path');
+app.set("/", "html");
+app.use(express.static(path.join(__dirname, "/")));
+app.use(express.json());
+
+
+app.use(express.urlencoded({extended: false}));
+app.get('/',(request,response)=>{
+    response.render("index")
+})
+
+app.listen(8080, () => {
+    console.log("Listening on http://localhost:8080");
+    });
